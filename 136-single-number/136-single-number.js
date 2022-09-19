c387617f9723e78a32f59aaacc6203a63f6b2306
Darik Mohammed <r.m.darik@gmail.com>
2022-09-19 16:44:10 +0300

    Time: 1136 ms (5.02%), Space: 43.4 MB (74.70%) - LeetHub

---
+++ 136-single-number/136-single-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    for (let i=0; i<nums.length ; i++){
+        if(nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i])) return nums[i]  
+    }
+};
