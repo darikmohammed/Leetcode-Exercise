3db370f1df5231c6ffa6d3cf66b5dbaac512ff30
Darik Mohammed <r.m.darik@gmail.com>
2022-10-10 16:54:11 +0300

    Time: 125 ms (9.71%), Space: 43.1 MB (69.86%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    // let pos = 0
+    // let target_number = target.charCodeAt(pos)
+    // let char = letters[0]
+    // let i = 1
+    // while(char &&char.charCodeAt(pos) <= target.charCodeAt(pos)){
+    //     char = letters[i]
+    //     i++
+    // }
+    // if(!char) return letters[0]
+    // letters.forEach(letter=>{
+    //    let letter_number = letter.charCodeAt(pos)
+    //     if(letter_number > target_number) {
+    //             if(char.charCodeAt(pos) > letter_number){
+    //                 char = letter
+    //             }
+    //     }
+    // })
+    // return char
+    for (let i = 0; i < letters.length; i++ ) {
+        if(letters[i].charCodeAt(0) > target.charCodeAt(0)) {
+            return letters[i]
+        } 
+    }
+    return letters[0]
+};
